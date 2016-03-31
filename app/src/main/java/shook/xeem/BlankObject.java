87068218f59df618891374216b527ac97cb97ae4
sHooK <shook@live.ru>
2016-03-31 23:55:47 +0500

    Changes in BlankObject class

--- app/src/main/java/shook/xeem/BlankObject.java
+++ app/src/main/java/shook/xeem/BlankObject.java
@@ -1,36 +1,55 @@
 package shook.xeem;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class BlankObject {
 
-    private String title = "";
-
-    private List<Question> questions = new ArrayList<Question>();
-
-    public int addQuestion(Question target) {
-        questions.add(target);
-        return 0;
+    // Empty blank constructor
+    public BlankObject (String title) {
+        this.questions = new ArrayList<Question>();
+        this.title = title;
     }
 
-    class Question {
+    // Blank variables
+    private String title = "";
+    private List<Question> questions;
 
-        public void Question (String text) {
+    public void setTitle (String newtitle) {
+        this.title = newtitle;
+    }
+
+    public void addQuestion (String text, String[] atts) {
+        this.questions.add(new Question(text, Arrays.asList(atts)));
+    }
+
+
+
+
+    // Question object
+    private class Question {
+
+        // Constructor
+        public Question (String text, List<String> atts) {
             this.text = text;
             this.answers = new ArrayList<Answer>();
+            this.attachments = new ArrayList<String>();
+            this.attachments.addAll(atts);
         }
 
+        // Class variables
         private String text;
-
         private List<Answer> answers; // = new ArrayList<Answer>();
+        private List<String> attachments;
 
         public int addAnswer(String text) {
             answers.add(new Answer(text));
             return answers.size();
         }
 
-        class Answer {
+        private class Answer {
             public Answer(String text) {
                 this.text = text;
             }
