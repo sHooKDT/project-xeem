e2410840065fc652718e77e6984418cfd7f89bde
sHooK <shook@live.ru>
2016-04-09 23:00:44 +0500

    Fixed some issues

--- app/src/main/java/shook/xeem/LoginActivity.java
+++ app/src/main/java/shook/xeem/LoginActivity.java
@@ -1,9 +1,25 @@
 package shook.xeem;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.OptionalPendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -11,9 +27,11 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
     }
 
-    public void loginClick (View v) {
+    public void clickEnter (View v) {
         startActivity(new Intent(this, MainActivity.class));
     }
+
 }
